8b850ab1695ca6e0e2860d16f6c9e086719036ae
timdereuse <timdereuse@gmail.com>
2021-02-15 21:43:47 -0500

    implementing pytorch default lstm w/attention

--- models/LSTM_autoregressive_model.py
+++ models/LSTM_autoregressive_model.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 
 
 class EncoderRNN(nn.Module):
-    def __init__(self, input_size, hidden_size):
+    def __init__(self, input_size, hidden_size, num_layers=3):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
+        self.num_layers = num_layers
 
         self.embedding = nn.Linear(input_size, hidden_size)
-        self.lstm = nn.LSTM(hidden_size, hidden_size, bidirectional=True, batch_first=True)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=self.num_layers, bidirectional=True, batch_first=True)
 
     def forward(self, input, hidden):
         embedded = nn.ReLU(self.embedding(input))
@@ -17,25 +18,42 @@ class EncoderRNN(nn.Module):
         return output, hidden
 
     def initHidden(self):
-        return torch.zeros(1, 1, self.hidden_size, device=device)
+        return torch.zeros(1, 1, self.hidden_size)
 
 
-class DecoderRNN(nn.Module):
-    def __init__(self, hidden_size, output_size):
-        super(DecoderRNN, self).__init__()
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, num_layers=3, dropout_p=0.1, max_length=1000):
+        super(AttnDecoderRNN, self).__init__()
         self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.dropout_p = dropout_p
+        self.max_length = max_length
 
-        self.embedding = nn.Embedding(output_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.softmax = nn.LogSoftmax(dim=1)
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.lstm = nn.LSTM(hidden_size, hidden_size, num_layers=self.num_layers, bidirectional=True, batch_first=True)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
 
-    def forward(self, input, hidden):
-        output = self.embedding(input).view(1, 1, -1)
-        output = F.relu(output)
+    def forward(self, input, hidden, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
+
+        attn_weights = nn.softmax(
+            self.attn(torch.cat((embedded[0], hidden[0]), 1)), dim=1)
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
+                                 encoder_outputs.unsqueeze(0))
+
+        output = torch.cat((embedded[0], attn_applied[0]), 1)
+        output = self.attn_combine(output).unsqueeze(0)
+
+        output = nn.relu(output)
         output, hidden = self.gru(output, hidden)
-        output = self.softmax(self.out(output[0]))
-        return output, hidden
+
+        output = nn.log_softmax(self.out(output[0]), dim=1)
+        return output, hidden, attn_weights
 
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
@@ -43,8 +61,16 @@ class DecoderRNN(nn.Module):
 
 teacher_forcing_ratio = 0.5
 
+hidden_size = 128
+lr = 0.01
 
-def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+encoder1 = EncoderRNN(4, 128)
+attn_decoder1 = AttnDecoderRNN(hidden_size, 3, dropout_p=0.1)
+encoder_optimizer = nn.optim.SGD(encoder.parameters(), lr=lr)
+decoder_optimizer = nn.optim.SGD(attn_decoder1.parameters(), lr=lr)
+
+
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=300):
     encoder_hidden = encoder.initHidden()
 
     encoder_optimizer.zero_grad()
