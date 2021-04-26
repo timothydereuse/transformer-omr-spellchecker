2726044438b53dc5a9ed9ff1987e4150e608e920
timdereuse <timdereuse@gmail.com>
2021-04-26 19:57:10 -0400

    new parameters for toy datasets

--- toy_datasets.py
+++ toy_datasets.py
@@ -5,13 +5,10 @@ import numpy as np
 
 class SequenceCopyDataset(Dataset):
 
-    def __init__(self, num_feats, num_seqs, seq_length, seq_period, freq_vary=0.2, seq_type='sine'):
-        """
-        @dset_fname - path to hdf5 file created by make_hdf5.py
-        """
+    def __init__(self, num_feats, num_seqs, seq_length, seq_period, phase_vary=0, freq_vary=0, sine=False):
+
         super(SequenceCopyDataset).__init__()
         self.seq_length = seq_length
-        self.seq_type = seq_type        # one of 'random', 'sine'
         self.seq_period = seq_period
         self.num_feats = num_feats
         self.num_seqs = num_seqs
@@ -20,19 +17,19 @@ class SequenceCopyDataset(Dataset):
         slope = np.linspace(1, seq_length, seq_length)
         data[:] = np.tile(slope, [num_feats, 1]).swapaxes(0, 1)
 
-        additive = np.random.normal(0, 10, [num_seqs, num_feats])
+        additive = np.random.normal(0, phase_vary, [num_seqs, num_feats])
         additive = np.repeat(additive[:, np.newaxis, :], seq_length, axis=1)
         data += additive
 
-        mult = np.random.uniform(1 - freq_vary, 1 + freq_vary, [num_seqs, num_feats])
+        mult = np.random.uniform(-freq_vary, freq_vary, [num_seqs, num_feats]) + 1
         mult = np.repeat(mult[:, np.newaxis, :], seq_length, axis=1)
-
         data *= mult
 
-        split_pt = 0 # num_feats // 2
-        data[:, :, split_pt:] = np.sin(data[:, :, split_pt:] * (2 * np.pi) / seq_period)
-        data[:, :, split_pt:] = (data[:, :, split_pt:] + 1.) / 2.
-        # data[:, :, :split_pt] = np.mod(data[:, :, :split_pt], seq_period) / seq_period
+        if sine:
+            data = np.sin(data * (2 * np.pi) / seq_period)
+        # data[:, :, split_pt:] = (data[:, :, split_pt:] + 1.) / 2.
+        else:
+            data = np.mod(data, seq_period) / seq_period
 
         self.data = data
 
@@ -42,18 +39,26 @@ class SequenceCopyDataset(Dataset):
     def __getitem__(self, index):
         return self.data[index]
 
+
 if __name__ == '__main__':
 
     from torch.utils.data import DataLoader
+    import matplotlib.pyplot as plt
 
     dset = SequenceCopyDataset(
-        num_feats=4,
+        num_feats=1,
         num_seqs=1200,
-        seq_length=100,
-        seq_period=12,
-        freq_vary=0.6)
+        seq_length=128,
+        seq_period=30,
+        phase_vary=0.01,
+        freq_vary=0.01)
 
     dloader = DataLoader(dset, 10)
 
     for batch in dloader:
         print(batch.shape)
+
+    x = batch.numpy()
+    for i in range(4):
+        plt.plot(x[i], label="line {i}")
+    plt.show()
