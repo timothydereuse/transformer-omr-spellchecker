1b019acd5ca0aea0bdf1caba8c11fa4862b8a738
timdereuse <timdereuse@gmail.com>
2021-05-20 14:16:07 -0400

    improving log file plot creation

--- plot_training_from_log_file.py
+++ plot_training_from_log_file.py
@@ -2,54 +2,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import re
+import os
 
 parser = argparse.ArgumentParser(description='Prints nice graphs of training and validation progress from al')
 parser.add_argument('logfile', default='', help='Log file to print training data from.')
 args = vars(parser.parse_args())
 
-log_file = args['logfile']
+loc = args['logfile']
+if loc[-4:] == '.log':
+    logs = [loc]
+else:
+    logs = [os.path.join(loc, x) for x in os.listdir(loc) if x[-4:] == '.log']
 
-with open(log_file) as f:
-    logs = f.readlines()
+for log_file in logs:
 
-logs = [x.strip() for x in logs if ('train_loss' in x) and ('s/epoch' in x)]
+    with open(log_file) as f:
+        logs = f.readlines()
 
-# make empty dictionary to hold all entries
-keys = logs[0].split('|')[1:]
-keys = [re.split(r'\s+', k)[1] for k in keys if k]
-dats = {k: [] for k in keys if k}
+    logs = [x.strip() for x in logs if ('train_loss' in x) and ('s/epoch' in x)]
 
-for line in logs:
-    entries = line.split('|')[1:]
-    for e in entries:
-        spl = re.split(r'\s+', e)
-        spl = [x for x in spl if x]
-        if not spl:
-            continue
-        dats[spl[0]].append(float(spl[1]))
+    # make empty dictionary to hold all entries
+    keys = logs[0].split('|')[1:]
+    keys = [re.split(r'\s+', k)[1] for k in keys if k]
+    dats = {k: [] for k in keys if k}
 
-fig, axs = plt.subplots(2, 1, figsize=(12, 6))
-epochs = np.arange(len(dats[keys[0]]))
+    for line in logs:
+        entries = line.split('|')[1:]
+        for e in entries:
+            spl = re.split(r'\s+', e)
+            spl = [x for x in spl if x]
+            if not spl:
+                continue
+            dats[spl[0]].append(float(spl[1]))
 
-axs[0].set_title('Training and Validation Loss')
-axs[0].plot(epochs, dats['train_loss'], c='black', label='Training')
-axs[0].plot(epochs, dats['val_loss'], c='red', label='Validation')
-axs[0].set_xlim([-0.02, max(epochs)])
-axs[0].set_ylim([-0.02, max(dats['val_loss'])])
-axs[0].set_ylabel('Loss')
-axs[0].set_xlabel('Epoch')
-axs[0].legend()
+    fig, axs = plt.subplots(2, 1, figsize=(12, 6))
+    epochs = np.arange(len(dats[keys[0]]))
 
-axs[1].set_title('F1 Score')
-axs[1].plot(epochs, dats['tr_f1'], c='black', label='Training')
-axs[1].plot(epochs, dats['val_f1'], c='red', label='Validation')
-axs[1].set_xlim([0, max(epochs)])
-axs[1].set_ylim([0, 1.02])
-axs[1].set_ylabel('F1 Score')
-axs[1].set_xlabel('Epoch')
-axs[1].legend()
+    axs[0].set_title('Training and Validation Loss')
+    axs[0].plot(epochs, dats['train_loss'], c='black', label='Training')
+    axs[0].plot(epochs, dats['val_loss'], c='red', label='Validation')
+    axs[0].set_xlim([-0.02, max(epochs)])
+    axs[0].set_ylim([-0.02, max(dats['val_loss'])])
+    axs[0].set_ylabel('Loss')
+    axs[0].set_xlabel('Epoch')
+    axs[0].legend()
 
-out_fname = f'{log_file[:-4]}_plot.png'
-fig.savefig(out_fname, bbox_inches='tight')
-plt.clf()
-plt.close(fig)
+    axs[1].set_title('F1 Score')
+    axs[1].plot(epochs, dats['tr_f1'], c='black', label='Training')
+    axs[1].plot(epochs, dats['val_f1'], c='red', label='Validation')
+    axs[1].set_xlim([0, max(epochs)])
+    axs[1].set_ylim([0, 1.02])
+    axs[1].set_ylabel('F1 Score')
+    axs[1].set_xlabel('Epoch')
+    axs[1].legend()
+
+    out_fname = f'{log_file[:-4]}_plot.png'
+    fig.savefig(out_fname, bbox_inches='tight')
+    plt.clf()
+    plt.close(fig)
