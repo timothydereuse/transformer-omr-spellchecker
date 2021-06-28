e4d55ce057b519ad80f272d656897ed2e56ab5a9
timdereuse <timdereuse@gmail.com>
2021-06-27 22:17:48 -0500

    fixing multiple bugs in class methods, cleaning

--- data_management/error_gen_logistic_regression.py
+++ data_management/error_gen_logistic_regression.py
@@ -1,19 +1,21 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
+import numpy as np
 
 class ErrorGenerator(object):
 
-        match_idx = 0
-        replace_idx = 1
-        insert_idx = 2
-        delete_index = 3
+    match_idx = 0
+    replace_idx = 1
+    insert_idx = 2
+    delete_index = 3
 
     def __init__(self, X, Y, ngram, ins_samples, repl_samples):
         self.enc = preprocessing.OneHotEncoder(sparse=True)
         self.enc.fit(X)
         X_one_hot = self.enc.transform(X)
         self.regression = LogisticRegression(max_iter=5000).fit(X_one_hot, Y)
+        self.ngram = ngram
         self.ins_samples = ins_samples
         self.repl_samples = repl_samples
 
@@ -22,7 +24,7 @@ class ErrorGenerator(object):
 
         seq = np.array(seq)
         errored_seq = []
-        gen_labels = [0 for _ in range(ngram)]
+        gen_labels = [0 for _ in range(self.ngram)]
 
         ins_samples = self.ins_samples
         repl_samples = self.repl_samples
@@ -31,7 +33,7 @@ class ErrorGenerator(object):
         
         i = 0
         while i < len(seq):
-            next_note = np.concatenate([last_labels[-ngram:], seq[i]])
+            next_note = np.concatenate([gen_labels[-self.ngram:], seq[i]])
             predictions = self.regression.predict_proba(self.enc.transform(next_note.reshape(1, -1)))[0]
             next_label = np.random.choice(len(predictions), p=predictions)
             gen_labels.append(next_label)
@@ -44,12 +46,16 @@ class ErrorGenerator(object):
                 errored_seq.append(seq[i] + rand_mod)
                 i += 1
             elif next_label == self.insert_idx: # INSERT
-                rand_ins = repl_samples[np.random.randint(len(ins_samples))]
+                rand_ins = ins_samples[np.random.randint(len(ins_samples))]
                 errored_seq.append(rand_ins)
             elif next_label == self.delete_index: # DELETE
                 i += 1
         
-        return errored_seq, gen_labels[ngram:]
+        return errored_seq, gen_labels[self.ngram:]
+
+    def err_seq_string(self, labels):
+        class_to_label = err_to_class = {0: 'O', 1: '~', 2: '+', 3: '-'}
+        return ''.join(err_to_class[x] for x in labels)
 
 
     # pad = np.zeros((seq.shape[0], 3)) # because trigrams
