c332c38ee6a476bc09aeba3e33a2b2cc698f400f
timdereuse <timdereuse@gmail.com>
2021-06-27 21:14:01 -0500

    logistic regression into a class

--- data_management/error_gen_logistic_regression.py
+++ data_management/error_gen_logistic_regression.py
@@ -2,34 +2,54 @@ from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn import preprocessing
 
-enc = preprocessing.OneHotEncoder(sparse=False)
-enc.fit(X)
-X_one_hot = enc.transform(X)
-regression = LogisticRegression(max_iter=1000).fit(X_one_hot, Y)
+class ErrorGenerator(object):
 
+        match_idx = 0
+        replace_idx = 1
+        insert_idx = 2
+        delete_index = 3
 
-def get_synthetic_error_sequence(seq, regression):
-    ngram = 3
-    num_ops = 4
-    ins_idx = 3
+    def __init__(self, X, Y, ngram, ins_samples, repl_samples):
+        self.enc = preprocessing.OneHotEncoder(sparse=True)
+        self.enc.fit(X)
+        X_one_hot = self.enc.transform(X)
+        self.regression = LogisticRegression(max_iter=5000).fit(X_one_hot, Y)
+        self.ins_samples = ins_samples
+        self.repl_samples = repl_samples
 
-    seq = np.array(seq)
-    last_labels = [0 for _ in range(ngram)]
+    def get_synthetic_error_sequence(self, seq):
+        num_ops = 4
 
-    # assemble note to run regression on: one note from the sequence and 3 previous labels
-    
-    i = 0
-    while i < len(seq):
-        next_note = np.concatenate([last_labels[-ngram:], seq[i]])
-        predictions = regression.predict_proba(enc.transform(next_note.reshape(1, -1)))[0]
-        next_label = np.random.choice(num_ops, p=predictions)
-        last_labels.append(next_label)
+        seq = np.array(seq)
+        errored_seq = []
+        gen_labels = [0 for _ in range(ngram)]
 
-        # if inserting into this sequence, then the previous note of the correct sequence stays the same.
-        if not next_label == ins_idx:
-            i += 1
-    
-    return last_labels
+        ins_samples = self.ins_samples
+        repl_samples = self.repl_samples
+
+        # assemble note to run regression on: one note from the sequence and 3 previous labels
+        
+        i = 0
+        while i < len(seq):
+            next_note = np.concatenate([last_labels[-ngram:], seq[i]])
+            predictions = self.regression.predict_proba(self.enc.transform(next_note.reshape(1, -1)))[0]
+            next_label = np.random.choice(len(predictions), p=predictions)
+            gen_labels.append(next_label)
+
+            if next_label == self.match_idx: # MATCH
+                errored_seq.append(seq[i])
+                i += 1
+            elif next_label == self.replace_idx: # REPLACE
+                rand_mod = repl_samples[np.random.randint(len(repl_samples))]
+                errored_seq.append(seq[i] + rand_mod)
+                i += 1
+            elif next_label == self.insert_idx: # INSERT
+                rand_ins = repl_samples[np.random.randint(len(ins_samples))]
+                errored_seq.append(rand_ins)
+            elif next_label == self.delete_index: # DELETE
+                i += 1
+        
+        return errored_seq, gen_labels[ngram:]
 
 
     # pad = np.zeros((seq.shape[0], 3)) # because trigrams
