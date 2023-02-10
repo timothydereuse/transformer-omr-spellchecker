af50861234e973105011e84e539d7b73e947f845
Timothy de Reuse <timdereuse@gmail.com>
2023-02-09 19:31:20 -0500

    fix testing with map location

--- run_tests_lstut_model.py
+++ run_tests_lstut_model.py
@@ -4,10 +4,10 @@ import model_params
 import torch
 
 if __name__ == "__main__":
-    model_path = "trained_models\lstut_best_LSTUT_TRIAL_0_(2023.01.10.17.06)_1-1-1-11-1-32-32.pt"
-    saved_model_info = torch.load(model_path)
+    model_path = "trained_models\lstut_best_NODE_LSTUT_FINAL3_0_(2023.02.08.00.38)_1-1-1-15-4-128-128.pt"
+    saved_model_info = torch.load(model_path, map_location=torch.device('cpu'))
 
-    params = model_params.Params('./param_sets/trial_lstut.json', False, 0)
+    params = model_params.Params('./param_sets/node_lstut.json', False, 0)
     device, num_gpus = tr_funcs.get_cuda_info()
 
     prep_model = PreparedLSTUTModel(params, saved_model_info['model_state_dict'])
