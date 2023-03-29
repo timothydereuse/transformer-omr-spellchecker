2aa77bc6ce360470ab8866b914d8d915757a6966
Timothy de Reuse <timdereuse@gmail.com>
2023-03-29 00:51:58 -0400

    seq build fix

--- data_management/semantic_to_agnostic.py
+++ data_management/semantic_to_agnostic.py
@@ -4,34 +4,13 @@ import os
 import math
 from collections import namedtuple
 from itertools import groupby
-
-AgnosticRecord = namedtuple('AgnosticRecord', ['agnostic_item', 'chord_idx', 'measure_idx', 'event_idx', 'part_idx'])
-
-class SequenceBuilder():
-
-    def __init__(self) -> None:
-        self.records = []
-        self.part_idx = 0
-        self.measure_idx = 0
-        self.event_idx = 0
-        self.chord_idx = 0
-
-    def add_record(self, token, chord_idx=None, measure_idx=None, event_idx=None, part_idx=None) -> None:
-        new_rec = AgnosticRecord(
-            token,
-            chord_idx if chord_idx else self.chord_idx,
-            measure_idx if measure_idx else self.measure_idx,
-            event_idx if event_idx else self.event_idx,
-            part_idx if part_idx else self.part_idx,
-        )
-        self.records.append(new_rec)
-
+from data_management.seq_builder import SequenceBuilder, AgnosticRecord, sorting_order
 
 def resolve_duration(d):
     dots = d.dots
 
     # if there's a durationexception when just trying to change the number
-    # of dots then, honestly, just return something - not sure how to
+    # of dots then just return something - honestly, not sure how to
     # handle this in any productive way.
     try:
         d.dots = 0
@@ -164,20 +143,6 @@ def resolve_tuplet_record(tuplet_record):
                 del tr[ks[x]]
 
     return to_insert
-
-
-def sorting_order(e):
-    # returns a tuple by which you can sort elements to get the order
-    # in which they should be processed by m21_part_to_agnostic
-    if hasattr(e, 'pitch'):
-        return (e.offset, e.pitch.diatonicNoteNum)
-    elif type(e) == m21.chord.Chord:
-        lowest_note = min(e.notes, key=lambda x: x.pitch.midi)
-        return (e.offset, lowest_note.pitch.diatonicNoteNum)
-    elif type(e) == m21.note.Rest:
-        return (e.offset, 1)
-    else:
-        return (e.offset, -100)
 
 
 def resolve_chord(e, current_clef):
@@ -479,6 +444,3 @@ if __name__ == '__main__':
         #     all_tokens.update(agnostic)
         agnostic = m21_parts_to_interleaved_agnostic(parts, remove=['+'])
         all_tokens.update([x.agnostic_item for x in agnostic])
-
-            
-# x for x in f['train'].keys() if 'musescore_misc' in x
---
+++ data_management/seq_builder.py
@@ -0,0 +1,37 @@
+from collections import namedtuple
+import music21 as m21
+
+AgnosticRecord = namedtuple('AgnosticRecord', ['agnostic_item', 'chord_idx', 'measure_idx', 'event_idx', 'part_idx'])
+
+class SequenceBuilder():
+
+    def __init__(self) -> None:
+        self.records = []
+        self.part_idx = 0
+        self.measure_idx = 0
+        self.event_idx = 0
+        self.chord_idx = 0
+
+    def add_record(self, token, chord_idx=None, measure_idx=None, event_idx=None, part_idx=None) -> None:
+        new_rec = AgnosticRecord(
+            token,
+            chord_idx if chord_idx else self.chord_idx,
+            measure_idx if measure_idx else self.measure_idx,
+            event_idx if event_idx else self.event_idx,
+            part_idx if part_idx else self.part_idx,
+        )
+        self.records.append(new_rec)
+        
+
+def sorting_order(e):
+    # returns a tuple by which you can sort elements to get the order
+    # in which they should be processed by m21_part_to_agnostic
+    if hasattr(e, 'pitch'):
+        return (e.offset, e.pitch.diatonicNoteNum)
+    elif type(e) == m21.chord.Chord:
+        lowest_note = min(e.notes, key=lambda x: x.pitch.midi)
+        return (e.offset, lowest_note.pitch.diatonicNoteNum)
+    elif type(e) == m21.note.Rest:
+        return (e.offset, 1)
+    else:
+        return (e.offset, -100)
