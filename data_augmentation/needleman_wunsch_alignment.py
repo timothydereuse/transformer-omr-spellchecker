1c98789e318d6403bad1d94a5162ab6625ebb005
Timothy de Reuse <timdereuse@gmail.com>
2023-06-19 17:26:37 -0400

    add banded needleman-wunsch alg

--- data_augmentation/needleman_wunsch_alignment.py
+++ data_augmentation/needleman_wunsch_alignment.py
@@ -1,4 +1,4 @@
-from typing import Match
+from typing import Match, Optional, Union
 import numpy as np
 from functools import partial
 from numba import njit
@@ -7,8 +7,16 @@ from numba.typed import List as numbaList
 default_match_weights = [3, -2]
 default_gap_penalties = [-2, -2, -1, -1]
 
+
 @njit
-def create_matrix(seq_a, seq_b, match_weights, gap_rules):
+def create_matrix(
+    seq_a: numbaList[int],
+    seq_b: numbaList[int],
+    match_weights: numbaList[int],
+    gap_rules: numbaList[int],
+    bands: float,
+) -> tuple[np.array, np.array, np.array, int]:
+
     gap_open_x, gap_open_y, gap_extend_x, gap_extend_y = gap_rules
 
     # y_mat and x_mat keep track of gaps in horizontal and vertical directions
@@ -32,8 +40,19 @@ def create_matrix(seq_a, seq_b, match_weights, gap_rules):
         x_mat[0][j] = gap_extend_y * j
         y_mat[0][j] = -1e100
 
+    biggest_seq_length = max(len_a, len_b)
+
     for i in range(1, len_a):
-        for j in range(1, len_b):
+        # how far along are we in dimension 1?
+        proportion = float(i) / len_a
+
+        center = int(np.round(proportion * len_b))
+        extend_length = int(np.round(biggest_seq_length * bands))
+
+        left_b = max(1, center - extend_length)
+        right_b = min(len_b, extend_length)
+
+        for j in range(left_b, right_b):
 
             # if np.all(seq_a[i-1] == seq_b[j-1]):
             if seq_a[i - 1] == seq_b[j - 1]:
@@ -42,37 +61,48 @@ def create_matrix(seq_a, seq_b, match_weights, gap_rules):
                 # match_score = np.sum(seq_a[i-1] != seq_b[j-1]) * match_weights[1]
                 match_score = match_weights[1]
 
-            mat_vals = np.array([mat[i-1][j-1], x_mat[i-1][j-1], y_mat[i-1][j-1]])
+            mat_vals = np.array(
+                [mat[i - 1][j - 1], x_mat[i - 1][j - 1], y_mat[i - 1][j - 1]]
+            )
             mat[i][j] = np.max(mat_vals) + match_score
-            # mat_ptr[i][j] = int(mat_vals.index(np.max(mat_vals)))
             mat_ptr[i][j] = np.argmax(mat_vals)
 
             # update matrix for y gaps
             y_mat_vals = np.array(
-                        [mat[i][j-1] + gap_open_y + gap_extend_y,
-                        x_mat[i][j-1] + gap_open_y + gap_extend_y,
-                        y_mat[i][j-1] + gap_extend_y])
+                [
+                    mat[i][j - 1] + gap_open_y + gap_extend_y,
+                    x_mat[i][j - 1] + gap_open_y + gap_extend_y,
+                    y_mat[i][j - 1] + gap_extend_y,
+                ]
+            )
 
             y_mat[i][j] = np.max(y_mat_vals)
-            # y_mat_ptr[i][j] = int(y_mat_vals.index(np.max(y_mat_vals)))
             y_mat_ptr[i][j] = np.argmax(y_mat_vals)
 
             # update matrix for x gaps
             x_mat_vals = np.array(
-                        [mat[i-1][j] + gap_open_x + gap_extend_x,
-                        x_mat[i-1][j] + gap_extend_x,
-                        y_mat[i-1][j] + gap_open_x + gap_extend_x])
+                [
+                    mat[i - 1][j] + gap_open_x + gap_extend_x,
+                    x_mat[i - 1][j] + gap_extend_x,
+                    y_mat[i - 1][j] + gap_open_x + gap_extend_x,
+                ]
+            )
 
             x_mat[i][j] = np.max(x_mat_vals)
-            # x_mat_ptr[i][j] = int(x_mat_vals.index(np.max(x_mat_vals)))
             x_mat_ptr[i][j] = np.argmax(x_mat_vals)
 
     return mat_ptr, x_mat_ptr, y_mat_ptr, mat[-1][-1]
 
 
-
-def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, ignore_case=True, verbose=False):
-    '''
+def perform_alignment(
+    transcript: list[int],
+    ocr: list[int],
+    match_weights: Optional[tuple[int, int]] = None,
+    gap_penalties: Union[tuple[int, int], tuple[int, int, int, int], None] = None,
+    bands: float = 1.0,
+    verbose: bool = False,
+) -> tuple[list[Union[int, str]], list[Union[int, str]], list[str], int]:
+    """
     @match_function must be a function that takes in two strings and returns a single integer:
         a positive integer for a "match," a negative integer for a "mismatch."
 
@@ -83,23 +113,7 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
     @ignore_case ensures that the default scoring method will treat uppercase and lowercase letters
         the same, by applying lower() before every comparison. this setting will be ignored if a
         callable function is passed into match_function.
-    '''
-
-    def default_score_method(a, b, weights, ignore_case):
-        if ignore_case:
-            a = a.lower()
-            b = b.lower()
-        return weights[0] if a == b else weights[1]
-
-    # if match_function is None:
-    #     weights = default_match_weights
-    #     scoring_method = partial(default_score_method, weights=weights, ignore_case=ignore_case)
-    # elif type(match_function) is list:
-    #     scoring_method = partial(default_score_method, weights=match_function, ignore_case=ignore_case)
-    # elif callable(match_function):
-    #     scoring_method = match_function
-    # else:
-    #     raise ValueError('gap_penalties argument {} invalid: must either be a list of 2 elements or a callable function that takes two elements'.format(match_function))
+    """
 
     if gap_penalties is None:
         gap_open_x, gap_open_y, gap_extend_x, gap_extend_y = default_gap_penalties
@@ -109,14 +123,18 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
         gap_open_x, gap_open_y = (gap_penalties[0], gap_penalties[0])
         gap_extend_x, gap_extend_y = (gap_penalties[1], gap_penalties[1])
     else:
-        raise ValueError('gap_penalties argument {} invalid: must be a list of either 4 or 2 elements'.format(gap_penalties))
+        raise ValueError(
+            "gap_penalties argument {} invalid: must be a list of either 4 or 2 elements".format(
+                gap_penalties
+            )
+        )
 
     if not len(transcript) > 0:
         print(ocr, transcript)
-        raise ValueError('Transcript must have more than 0 elements.')
+        raise ValueError("Transcript must have more than 0 elements.")
     if not len(ocr) > 0:
         print(ocr, transcript)
-        raise ValueError('OCR sequence must have more than 0 elements.')
+        raise ValueError("OCR sequence must have more than 0 elements.")
 
     transcript = transcript + [transcript[-1]]
     ocr = ocr + [ocr[-1]]
@@ -126,7 +144,9 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
     ocr_numba = numbaList(ocr)
     match_weights = numbaList(match_weights)
     gap_penalties = numbaList(gap_penalties)
-    mat_ptr, x_mat_ptr, y_mat_ptr, total_score = create_matrix(transcript_numba, ocr_numba, match_weights, gap_penalties)
+    mat_ptr, x_mat_ptr, y_mat_ptr, total_score = create_matrix(
+        transcript_numba, ocr_numba, match_weights, gap_penalties, bands
+    )
 
     # TRACEBACK
     # which matrix we're in tells us which direction to head back (diagonally, y, or x)
@@ -145,10 +165,10 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
     # start it off. we are forcibly aligning the final characters. this is not ideal.
     tra_align += [transcript[xpt]]
     ocr_align += [ocr[ypt]]
-    align_record += ['O'] if np.all(transcript[xpt] == ocr[ypt]) else ['~']
+    align_record += ["O"] if np.all(transcript[xpt] == ocr[ypt]) else ["~"]
 
     # start at bottom-right corner and work way up to top-left
-    while(xpt > 0 and ypt > 0):
+    while xpt > 0 and ypt > 0:
 
         pt_record += str(int(mpt))
 
@@ -159,7 +179,9 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
             # added_text = str(transcript[xpt - 1]) + ' ' + str(ocr[ypt - 1])
 
             # determine if this diagonal step was a match or a mismatch
-            align_record.append('O' if np.all(transcript[xpt - 1] == ocr[ypt - 1]) else '~')
+            align_record.append(
+                "O" if np.all(transcript[xpt - 1] == ocr[ypt - 1]) else "~"
+            )
 
             mpt = mat_ptr[xpt][ypt]
             xpt -= 1
@@ -168,20 +190,20 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
         # case if current cell is reachable horizontally
         elif mpt == 1:
             tra_align.append(transcript[xpt - 1])
-            ocr_align.append('_')
+            ocr_align.append("_")
             # added_text = str(transcript[xpt - 1]) + ' _'
 
-            align_record.append('-')
+            align_record.append("-")
             mpt = x_mat_ptr[xpt][ypt]
             xpt -= 1
 
         # case if current cell is reachable vertically
         elif mpt == 2:
-            tra_align.append('_')
+            tra_align.append("_")
             ocr_align.append(ocr[ypt - 1])
             # added_text = '_ ' + str(ocr[ypt - 1])
 
-            align_record.append('+')
+            align_record.append("+")
             mpt = y_mat_ptr[xpt][ypt]
             ypt -= 1
 
@@ -193,15 +215,15 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
 
     # print(xpt, ypt)
     while ypt > 0:
-        tra_align.append('_')
+        tra_align.append("_")
         ocr_align.append(ocr[ypt - 1])
-        align_record.append('-')
+        align_record.append("-")
         ypt -= 1
 
     while xpt > 0:
-        ocr_align.append('_')
+        ocr_align.append("_")
         tra_align.append(transcript[xpt - 1])
-        align_record.append('+')
+        align_record.append("+")
         xpt -= 1
 
     # reverse all records, since we obtained them by traversing the matrices from the bottom-right
@@ -212,37 +234,34 @@ def perform_alignment(transcript, ocr, match_weights=None, gap_penalties=None, i
 
     if verbose:
         for n in range(len(tra_align)):
-            line = '{} {} {}'
+            line = "{} {} {}"
             print(line.format(tra_align[n], ocr_align[n], align_record[n]))
 
     return tra_align, ocr_align, align_record, total_score
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    seq1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit '
-    seq2 = 'LoLO LOrem ipsum fipsudolor ..... sit eamet, c.nnr adizisdcing eelitellit'
+    seq1 = "The Needleman–Wunsch algorithm is an algorithm used in bioinformatics to align protein or nucleotides sequences. It was an early application of dynamic programming to compare biological sequences. The algorithm was developed by Saul B. Needleman and Christian D. Wunsch and published in 1970."
+    seq2 = "The Needleman–Wunsch algorithm is an algorithm used to align protein or nucleotide sequences. It was one of the first applications of dynamic programming to the comparison of biological sequences. The algorithm was developed by Saul B. Needleman and Christian D. Wunsch and published in 1970."
     match_weights = [3, -2]
     gap_penalties = [-1, -1, -1, -1]
 
-    # seq1 = [seq1[2*x] + seq1[2*x + 1] for x in range(len(seq1) // 2)]
-    # seq2 = [seq2[2*x] + seq2[2*x + 1] for x in range(len(seq2) // 2)]
-
     seq1 = list([ord(x) for x in seq1])
     seq2 = list([ord(x) for x in seq2])
-    # seq1 = [np.array([ord(x)]) for x in seq1]
-    # seq2 = [np.array([ord(x)]) for x in seq2]
 
-    a, b, align_record, score = perform_alignment(seq1, seq2, match_weights, gap_penalties, ignore_case=True)
+    a, b, align_record, score = perform_alignment(
+        seq1, seq2, match_weights, gap_penalties, bands=0.7, verbose=True
+    )
 
-    sa = ''
-    sb = ''
+    sa = ""
+    sb = ""
 
     for n in range(len(a)):
-        spacing = str(max(len(a[n]), len(b[n])))
-        sa += ('{:' + spacing + 's}').format(str(a[n]))
-        sb += ('{:' + spacing + 's}').format(str(b[n]))
+        # spacing = str(max(len(a[n]), len(b[n])))
+        spacing = "1"
+        sa += ("{:" + spacing + "s}").format(chr(a[n]) if type(a[n]) is int else "_")
+        sb += ("{:" + spacing + "s}").format(chr(b[n]) if type(b[n]) is int else "_")
 
     print(sa)
     print(sb)
-
